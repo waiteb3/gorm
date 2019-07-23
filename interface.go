7c9b2a24046924096f2b0d9c3b2fd53fb286c7ea
Brandon Waite <waiteb3@users.noreply.github.com>
2019-07-23 11:37:26 -0400

    Add Context functions to SQLCommon interface

--- interface.go
+++ interface.go
@@ -8,9 +8,13 @@ import (
 // SQLCommon is the minimal database connection functionality gorm requires.  Implemented by *sql.DB.
 type SQLCommon interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	Prepare(query string) (*sql.Stmt, error)
+	PrepareContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	QueryRow(query string, args ...interface{}) *sql.Row
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 }
 
 type sqlDb interface {
